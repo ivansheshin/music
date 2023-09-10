1c9315813018e0c05b81c80e8347b8d3fc83aeab
ivansheshin <ivashesh@gmail.com>
2023-09-10 14:02:14 +0400

    update logo

--- src/components/TheHeader/index.tsx
+++ src/components/TheHeader/index.tsx
@@ -4,6 +4,7 @@ import styles from './TheHeader.module.css'
 export default function TheHeader() {
   return (
       <header className={styles.header}>
+        <span className={styles['logo-text']}>music</span>
         <MButton title="Home"/>
       </header>
   )
